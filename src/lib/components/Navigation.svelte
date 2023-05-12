1e191d4710ec7222d8df943525e0bd2f3bc8c45b
Magnus Karström <magnus@yesweb.se>
2023-05-12 08:18:12 +0200

    Init

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { drawerStore } from '@skeletonlabs/skeleton';
+
+	function closeDrawer(): void {
+		drawerStore.close();
+	}
+</script>
+
+<nav class="list-nav p-4">
+	<ul>
+		<li><a href="/" on:click={closeDrawer}>Main</a></li>
+		<li><a href="/1" on:click={closeDrawer}>Page 1</a></li>
+		<li><a href="/2" on:click={closeDrawer}>Page 2</a></li>
+		<li><a href="/3" on:click={closeDrawer}>Page 3</a></li>
+	</ul>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { AppShell, AppBar, Avatar, Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/components/Navigation.svelte';
+
+	function drawerOpen(): void {
+		drawerStore.open();
+	}
+</script>
+
+<Drawer>
+	<Navigation />
+</Drawer>
+
+<AppShell slotSidebarLeft="w-0 md:w-52 bg-surface-500/10">
+	<svelte:fragment slot="header"
+		><AppBar regionRowHeadline="mx-auto">
+			<svelte:fragment slot="lead">
+				<button class="btn btn-sm mr-4 md:hidden" on:click={drawerOpen}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="fill-token"
+						viewBox="0 0 24 24"
+						class="inline-block w-5 h-5 stroke-current"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						/></svg
+					>
+				</button>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<!-- Find all the Avatar-props here: https://www.skeleton.dev/components/avatars -->
+				<Avatar
+					width="w-10"
+					initials="MK"
+					border="border-4 border-surface-300-600-token hover:!border-primary-500"
+					cursor="cursor-pointer"
+				/>
+			</svelte:fragment>
+			<svelte:fragment slot="headline">(headline)</svelte:fragment>
+		</AppBar></svelte:fragment
+	>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
+	<!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
+	<!-- Router Slot -->
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- <svelte:fragment slot="footer">Footer</svelte:fragment> -->
+</AppShell>
